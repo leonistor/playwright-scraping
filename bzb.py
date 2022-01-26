afa43c47364401be8bbb739b70a4dd165ea22424
leonistor <leo.nistor@gmail.com>
2022-01-26 21:05:47 +0200

    bzb works

--- bzb.py
+++ bzb.py
@@ -1,9 +1,11 @@
 import asyncio
-from webbrowser import get
 import aiohttp
+import aiofiles
+import os
 
 from structlog import get_logger
 from tap import Tap
+from jsonlines import Writer
 
 from bs4 import BeautifulSoup
 from bs4.element import Tag
@@ -28,11 +30,22 @@ def get_content(soup: BeautifulSoup | Tag, selector, default="") -> str:
         return default
 
 
+async def save_image(session, outdir: str, image_url: str, image_file: str):
+    async with session.get(image_url, headers=UA_HEADERS) as resp:
+        log.debug("image", url=image_url, file=image_file, status=resp.status)
+        if resp.status == 200:
+            fimg = await aiofiles.open(f"{outdir}/{image_file}", "wb")
+            await fimg.write(await resp.read())
+            await fimg.close()
+
+
 async def get_article(
     url: str,
     session: aiohttp.ClientSession,
     section_title: str,
     pub_date: str,
+    outdir: str,
+    writer: Writer,
 ):
     article = {}
     article["url"] = url
@@ -44,12 +57,34 @@ async def get_article(
             soup = BeautifulSoup(await resp.text(), "lxml")
             article["title"] = get_content(soup, "h1")
             article["author"] = get_content(soup, ".a_author > span:nth-child(1)")
+            text_tag = soup.select_one(".txt_articol > p:nth-child(2)")
+            if text_tag is not None:
+                text = str(text_tag)
+            else:
+                text = ""
+            article["text"] = text
+            # article image
+            img = soup.select_one(".article_img")
+            if (img is not None) and img.has_attr("src"):
+                src = str(img["src"])
+                img_file = src.split("/")[-1]
+                img_url = f"{BASE_URL}{src}"
+                article["img"] = img_file
+                await save_image(
+                    session,
+                    outdir,
+                    image_url=img_url,
+                    image_file=img_file,
+                )
+
         else:
+            # oops
             article["error"] = resp.status
-    log.debug("article", article=article)
+    # log.debug("article", article=article)
+    writer.write(article)
 
 
-async def main(edition_id: int):
+async def main(edition_id: int, outdir: str, writer: Writer):
     edition = {}
     edition["id"] = str(edition_id)
 
@@ -80,6 +115,8 @@ async def main(edition_id: int):
                             session,
                             section_title,
                             pub_date=edition_date,
+                            outdir=outdir,
+                            writer=writer,
                         )
                     )
                     tasks.append(task)
@@ -92,4 +129,15 @@ if __name__ == "__main__":
         edition_id: int = 7425
 
     args = SimpleArgParser().parse_args()
-    asyncio.run(main(args.edition_id))
+
+    outdir = f"output/bzb/{args.edition_id}"
+    os.makedirs(outdir, exist_ok=True)
+    fout = open(outdir + f"/articles-bzb-{args.edition_id}.jsonl", "w")
+    writer = Writer(fout)
+    asyncio.run(
+        main(
+            edition_id=args.edition_id,
+            outdir=outdir,
+            writer=writer,
+        )
+    )
