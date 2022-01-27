bc70375ed14eb52c8acdde96c7c205d799f84081
leonistor <leo.nistor@gmail.com>
2022-01-27 14:05:29 +0200

    more work

--- ZIARE.md
+++ ZIARE.md
@@ -7,6 +7,14 @@ arhiva: https://bzb.ro/arhiva/2020#archive
 - 2021: `for i in (seq 7364 7607); python bzb.py --edition $i; end`
 - 2020: `for i in (seq 7119 7363); python bzb.py --edition $i; end`
 - 2019: `for i in (seq 6876 7118); python bzb.py --edition $i; end`
+- 2018: `for i in (seq 6633 6875); python bzb.py --edition $i; end`
+- 2017: `for i in (seq 6393 6632); python bzb.py --edition $i; end`
+- 2016: `for i in (seq 6144 6392); python bzb.py --edition $i; end`
+- 2015: `for i in (seq 5887 6143); python bzb.py --edition $i; end`
+- 2014: `for i in (seq 5592 5886); python bzb.py --edition $i; end`
+- 2013: `for i in (seq 5293 5591); python bzb.py --edition $i; end`
+- 2012: `for i in (seq 4995 5292); python bzb.py --edition $i; end`
+- 2011: `for i in (seq 4694 4994); python bzb.py --edition $i; end`
 
 ## Contemporanul
 
--- contemporanul.py
+++ contemporanul.py
@@ -1,4 +1,5 @@
 import asyncio
+from distutils.log import debug
 import aiohttp
 import aiofiles
 import os
@@ -12,23 +13,88 @@ from bs4.element import Tag
 
 """ const """
 MAX_NUM_CONNECTIONS = 5
-BASE_URL = "https://www.contemporanul.ro/numere/"
+BASE_URL = "https://www.contemporanul.ro/"
 UA_HEADERS = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0"
 }
-
+LNAMES = {
+    "01": "ianuarie",
+    "02": "februarie",
+    "03": "martie",
+    "04": "aprilie",
+    "05": "mai",
+    "06": "iunie",
+    "07": "iulie",
+    "08": "august",
+    "09": "septembrie",
+    "10": "octombrie",
+    "11": "noiembrie",
+    "12": "decembrie",
+}  # "luni" (months) names
 
 """ globals """
 log = get_logger()
+
+
+def get_content(soup: BeautifulSoup | Tag, selector, default="") -> str:
+    elem = soup.select_one(selector=selector)
+    if elem is not None:
+        return elem.get_text()
+    else:
+        return default
+
+
+async def save_image(session, outdir: str, image_url: str, image_file: str):
+    async with session.get(image_url, headers=UA_HEADERS) as resp:
+        # log.debug("image", url=image_url, file=image_file, status=resp.status)
+        if resp.status == 200:
+            fimg = await aiofiles.open(f"{outdir}/img/{image_file}", "wb")
+            await fimg.write(await resp.read())
+            await fimg.close()
+
+
+def get_article_links(soup: BeautifulSoup):
+    pass
+
+
+async def get_luna(luna: str, an: int, outdir: str):
+    fout = open(outdir + f"/contemporanul-{an}-{luna}.jsonl", "w")
+    writer = Writer(fout)
+    # writer.write({"an": an, "luna": luna, "test": "yes"})
+    # a "luna" (edition) has about 3 pages listing links to articles
+    luna_url = f"{BASE_URL}numere/nr-{luna}-{LNAMES[luna]}-{an}"
+    log.debug("luna", when={"an": an, "luna": luna, "url": luna_url})
+    # get article links from all the pages of edition
+    article_links = []
+    num_pages = None
+    # get  first page
+    connector = aiohttp.TCPConnector(limit_per_host=MAX_NUM_CONNECTIONS)
+    async with aiohttp.ClientSession(connector=connector) as session:
+        async with session.get(luna_url, headers=UA_HEADERS) as resp:
+            soup = BeautifulSoup(await resp.text(), "lxml")
+    # get num_pages from 'Page x of y'
+    span_pages = get_content(
+        soup,
+        "#main-content > div.content-wrap > div > div.pagination > span.pages",
+    )
+    span_num_pages = span_pages.split(" ")[-1]
+    try:
+        num_pages = int(span_num_pages)
+    except ValueError as e:
+        log.error("oops, invalid num_pages", span_pages=span_pages)
+        raise e
+    log.debug("num_pages", num_pages=num_pages)
+    # TODO: from first page eliminate first item, is archive
+    # for this and next pages extend article_links
 
 
 async def main(an: int, outdir: str):
     luni = []
     for i in range(1, 13):
         luni.append("{:02d}".format(i))
-    # TODO: fout si writer pe luni
-    fout = open(outdir + f"/articles-contemporanul-{an}.jsonl", "w")
-    writer = Writer(fout)
+    # TODO: debug
+    for luna in luni[:3]:
+        await get_luna(luna, an, outdir)
 
 
 if __name__ == "__main__":
