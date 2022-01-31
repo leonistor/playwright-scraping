6abc0096f9a76fee9fdbf76dc140afe1f7bd3d2c
leonistor <leo.nistor@gmail.com>
2022-01-31 13:33:58 +0200

    pytest async

--- cpasync/pytest_async/test_hello_async.py
+++ cpasync/pytest_async/test_hello_async.py
@@ -14,3 +14,29 @@ from hello_async import say_hello
 @pytest.mark.asyncio
 async def test_say_hello(name):
     await say_hello(name)
+
+
+class TestSayHelloThrowsExcptions:
+    @pytest.mark.parametrize(
+        "name",
+        [
+            "",
+        ],
+    )
+    @pytest.mark.asyncio
+    async def test_say_hello_value_error(self, name):
+        with pytest.raises(ValueError):
+            await say_hello(name)
+
+    @pytest.mark.parametrize(
+        "name",
+        [
+            19,
+            {"ana": "mere"},
+            [],
+        ],
+    )
+    @pytest.mark.asyncio
+    async def test_say_hello_type_error(self, name):
+        with pytest.raises(TypeError):
+            await say_hello(name)
