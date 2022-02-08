c85632160e685f4e734f6b115d7768c97a1e692d
leonistor <leo.nistor@gmail.com>
2022-02-08 17:44:44 +0200

    mytex get urls worked, non-unique

--- mytex.py
+++ mytex.py
@@ -33,16 +33,12 @@ def main():
     # read categories start pages from file:
     with open(CATEGORIES_FILES, "r") as f:
         categories = f.read().splitlines()
-    # reset output file
-    fout = open(OUTFILE, "w")
-    fout.truncate()
 
-    url = f"{categories[CURRENT_CATEGORY]}#p3"
-    with sync_playwright() as pw:
+    with sync_playwright() as pw, open(OUTFILE, "w") as fout:
         # prepare playwright
         browser = pw.firefox.launch(
-            headless=False,
-            slow_mo=3,
+            headless=True,
+            # slow_mo=3,
             proxy={
                 "server": "http://intelnuc:3129",
             },
@@ -54,53 +50,54 @@ def main():
         ctx.set_default_timeout(CONTEXT_TIMEOUT)
         page = ctx.new_page()
 
-        # category start page
-        page.goto(url)
-        page.wait_for_load_state(state="domcontentloaded")
-
-        # scroll to bottom
-        delay(page)
-        footer = page.locator(".customfooter")
-        footer.scroll_into_view_if_needed(timeout=0)
-        more_btn = page.locator("input#btnAiMyTexSearchLoadMore")
-        more_btn.wait_for(state="attached")
-        save_articles_urls(page, fout)
-
-        # category second page: from 'Mai multe...' button
-        try:
-            more_btn.scroll_into_view_if_needed()
-            delay(page)
-            page.click("input#btnAiMyTexSearchLoadMore")
+        for url in categories:
+            log.info(f"PROCESSING CATEG {url}")
+            # category start page
+            page.goto(url)
             page.wait_for_load_state(state="domcontentloaded")
-            save_articles_urls(page, fout)
-        except Exception as e:
-            log.error(f"url: {page.url}, error: {e}")
-            raise e
 
-        # rest of page: click on bottom pagination '>'
-        has_next_page = True
-        page_count = 3
-        # for _ in range(10):
-        while has_next_page:
+            # scroll to bottom
+            delay(page)
+            footer = page.locator(".customfooter")
+            footer.scroll_into_view_if_needed(timeout=0)
+            more_btn = page.locator("input#btnAiMyTexSearchLoadMore")
+            more_btn.wait_for(state="attached")
+            save_articles_urls(page, fout)
+
+            # category second page: from 'Mai multe...' button
             try:
+                more_btn.scroll_into_view_if_needed()
                 delay(page)
-                next_page_arrow = page.locator("li.pagination-next a.pagenav")
-                next_page_arrow.scroll_into_view_if_needed()
-                page.click("li.pagination-next a.pagenav")
+                page.click("input#btnAiMyTexSearchLoadMore")
                 page.wait_for_load_state(state="domcontentloaded")
                 save_articles_urls(page, fout)
-                page_count += 1
             except Exception as e:
-                has_next_page = False
                 log.error(f"url: {page.url}, error: {e}")
-                log.info(f"done at page {page_count}")
-                # raise e
-                pass
+                raise e
+
+            # rest of page: click on bottom pagination '>'
+            has_next_page = True
+            page_count = 3
+            # for _ in range(10):
+            while has_next_page:
+                try:
+                    delay(page)
+                    next_page_arrow = page.locator("li.pagination-next a.pagenav")
+                    next_page_arrow.scroll_into_view_if_needed()
+                    page.click("li.pagination-next a.pagenav")
+                    page.wait_for_load_state(state="domcontentloaded")
+                    save_articles_urls(page, fout)
+                    page_count += 1
+                except Exception as e:
+                    has_next_page = False
+                    log.error(f"url: {page.url}, error: {e}")
+                    log.info(f"done at page {page_count}")
+                    # raise e
+                    pass
+        # - for categories
 
         browser.close()
-
-    # cleanup
-    fout.close()
+        # - sync_playwright, fout
 
     # -
 
